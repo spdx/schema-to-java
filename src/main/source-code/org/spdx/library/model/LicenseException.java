81dfda51d427813734faf0ad3573101a1cb1d934
anisha <anishasrivastava1411@gmail.com>
2020-07-06 16:49:11 +0530

    Error resolved and created GlobalCustomization.xml file

--- src/main/source-code/org/spdx/library/model/LicenseException.java
+++ src/main/source-code/org/spdx/library/model/LicenseException.java
@@ -17,109 +17,109 @@ public class LicenseException
     private String licenseExceptionText;
 
     /** 
-     * Get the 'example' element value. Text for examples in describing an SPDX element.
-     * 
-     * @return value
-     */
+    * Get the 'example' element value. Text for examples in describing an SPDX element.
+    * 
+    * @return value
+    */
     public String getExample() {
         return example;
     }
 
     /** 
-     * Set the 'example' element value. Text for examples in describing an SPDX element.
-     * 
-     * @param example
-     */
+    * Set the 'example' element value. Text for examples in describing an SPDX element.
+    * 
+    * @param example
+    */
     public void setExample(String example) {
         this.example = example;
     }
 
     /** 
-     * Get the list of 'seeAlso' element items.
-     * 
-     * @return list
-     */
+    * Get the list of 'seeAlso' element items.
+    * 
+    * @return list
+    */
     public List<String> getSeeAlsoList() {
         return seeAlsoList;
     }
 
     /** 
-     * Set the list of 'seeAlso' element items.
-     * 
-     * @param list
-     */
+    * Set the list of 'seeAlso' element items.
+    * 
+    * @param list
+    */
     public void setSeeAlsoList(List<String> list) {
         seeAlsoList = list;
     }
 
     /** 
-     * Get the 'name' element value. Identify name of this SpdxElement.
-     * 
-     * @return value
-     */
+    * Get the 'name' element value. Identify name of this SpdxElement.
+    * 
+    * @return value
+    */
     public String getName() {
         return name;
     }
 
     /** 
-     * Set the 'name' element value. Identify name of this SpdxElement.
-     * 
-     * @param name
-     */
+    * Set the 'name' element value. Identify name of this SpdxElement.
+    * 
+    * @param name
+    */
     public void setName(String name) {
         this.name = name;
     }
 
     /** 
-     * Get the 'licenseExceptionId' element value. Short form license exception identifier in Appendix I.2 of the SPDX specification.
-     * 
-     * @return value
-     */
+    * Get the 'licenseExceptionId' element value. Short form license exception identifier in Appendix I.2 of the SPDX specification.
+    * 
+    * @return value
+    */
     public String getLicenseExceptionId() {
         return licenseExceptionId;
     }
 
     /** 
-     * Set the 'licenseExceptionId' element value. Short form license exception identifier in Appendix I.2 of the SPDX specification.
-     * 
-     * @param licenseExceptionId
-     */
+    * Set the 'licenseExceptionId' element value. Short form license exception identifier in Appendix I.2 of the SPDX specification.
+    * 
+    * @param licenseExceptionId
+    */
     public void setLicenseExceptionId(String licenseExceptionId) {
         this.licenseExceptionId = licenseExceptionId;
     }
 
     /** 
-     * Get the 'comment' element value.
-     * 
-     * @return value
-     */
+    * Get the 'comment' element value.
+    * 
+    * @return value
+    */
     public String getComment() {
         return comment;
     }
 
     /** 
-     * Set the 'comment' element value.
-     * 
-     * @param comment
-     */
+    * Set the 'comment' element value.
+    * 
+    * @param comment
+    */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
     /** 
-     * Get the 'licenseExceptionText' element value. Full text of the license exception.
-     * 
-     * @return value
-     */
+    * Get the 'licenseExceptionText' element value. Full text of the license exception.
+    * 
+    * @return value
+    */
     public String getLicenseExceptionText() {
         return licenseExceptionText;
     }
 
     /** 
-     * Set the 'licenseExceptionText' element value. Full text of the license exception.
-     * 
-     * @param licenseExceptionText
-     */
+    * Set the 'licenseExceptionText' element value. Full text of the license exception.
+    * 
+    * @param licenseExceptionText
+    */
     public void setLicenseExceptionText(String licenseExceptionText) {
         this.licenseExceptionText = licenseExceptionText;
     }
--- src/main/source-code/org/spdx/library/model/SpdxElement.java
+++ src/main/source-code/org/spdx/library/model/SpdxElement.java
@@ -16,91 +16,91 @@ public class SpdxElement
     private String SPDXID;
 
     /** 
-     * Get the 'name' element value. Identify name of this SpdxElement.
-     * 
-     * @return value
-     */
+    * Get the 'name' element value. Identify name of this SpdxElement.
+    * 
+    * @return value
+    */
     public String getName() {
         return name;
     }
 
     /** 
-     * Set the 'name' element value. Identify name of this SpdxElement.
-     * 
-     * @param name
-     */
+    * Set the 'name' element value. Identify name of this SpdxElement.
+    * 
+    * @param name
+    */
     public void setName(String name) {
         this.name = name;
     }
 
     /** 
-     * Get the 'comment' element value.
-     * 
-     * @return value
-     */
+    * Get the 'comment' element value.
+    * 
+    * @return value
+    */
     public String getComment() {
         return comment;
     }
 
     /** 
-     * Set the 'comment' element value.
-     * 
-     * @param comment
-     */
+    * Set the 'comment' element value.
+    * 
+    * @param comment
+    */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
     /** 
-     * Get the list of 'relationship' element items. Defines a relationship between two SPDX elements. The SPDX element may be a Package, File, or SpdxDocument.
-     * 
-     * @return list
-     */
+    * Get the list of 'relationship' element items. Defines a relationship between two SPDX elements. The SPDX element may be a Package, File, or SpdxDocument.
+    * 
+    * @return list
+    */
     public List<Relationship> getRelationshipList() {
         return relationshipList;
     }
 
     /** 
-     * Set the list of 'relationship' element items. Defines a relationship between two SPDX elements. The SPDX element may be a Package, File, or SpdxDocument.
-     * 
-     * @param list
-     */
+    * Set the list of 'relationship' element items. Defines a relationship between two SPDX elements. The SPDX element may be a Package, File, or SpdxDocument.
+    * 
+    * @param list
+    */
     public void setRelationshipList(List<Relationship> list) {
         relationshipList = list;
     }
 
     /** 
-     * Get the list of 'annotation' element items. Provide additional information about an SpdxElement.
-     * 
-     * @return list
-     */
+    * Get the list of 'annotation' element items. Provide additional information about an SpdxElement.
+    * 
+    * @return list
+    */
     public List<Annotation> getAnnotationList() {
         return annotationList;
     }
 
     /** 
-     * Set the list of 'annotation' element items. Provide additional information about an SpdxElement.
-     * 
-     * @param list
-     */
+    * Set the list of 'annotation' element items. Provide additional information about an SpdxElement.
+    * 
+    * @param list
+    */
     public void setAnnotationList(List<Annotation> list) {
         annotationList = list;
     }
 
     /** 
-     * Get the 'SPDXID' element value.
-     * 
-     * @return value
-     */
+    * Get the 'SPDXID' element value.
+    * 
+    * @return value
+    */
     public String getSPDXID() {
         return SPDXID;
     }
 
     /** 
-     * Set the 'SPDXID' element value.
-     * 
-     * @param SPDXID
-     */
+    * Set the 'SPDXID' element value.
+    * 
+    * @param SPDXID
+    */
     public void setSPDXID(String SPDXID) {
         this.SPDXID = SPDXID;
     }
