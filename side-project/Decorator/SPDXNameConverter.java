5c2bd36eed66e46036b0b1d7bdf8df3b51499d9f
Anisha <49086647+AnishaSrivastava1411@users.noreply.github.com>
2020-08-28 02:01:25 +0530

    Update SPDXNameConverter.java

--- side-project/Decorator/SPDXNameConverter.java
+++ side-project/Decorator/SPDXNameConverter.java
@@ -9,17 +9,13 @@ public class SPDXNameConverter extends DefaultNameConverter {
 		if(text.startsWith("fileType_"))
 			text=text.substring("fileType_".length()); 
 		if(text.startsWith("annotationType_"))
-	    	text= text.substring("annotationType_".length()); 
- 		if(text.startsWith("checksumAlgorithm_"))
+			text= text.substring("annotationType_".length()); 
+		if(text.startsWith("checksumAlgorithm_"))
 			text=text.substring("checksumAlgorithm_".length()); 
-        if(text.startsWith("referenceCategory_"))
-	    	text= text.substring("referenceCategory_".length()); 
-	    if(text.startsWith("relationshipType_"))
-		    text= text.substring("relationshipType_".length()); 
-		
-	   	return super.toConstantName(text);
-		  
-     }
-	
-	
+		if(text.startsWith("referenceCategory_"))
+			text= text.substring("referenceCategory_".length()); 
+		if(text.startsWith("relationshipType_"))
+			text= text.substring("relationshipType_".length()); 
+		return super.toConstantName(text);
+	}
 }
